6092ecf459ab833b71454e64f3bcb3caeb69cfbc
Vandborg <jesper@vandborg.net>
2013-02-26 16:16:12 +0100

    This doesnt work!

--- src/ucn/datamatiker/vandborg/webuntis/MainActivity.java
+++ src/ucn/datamatiker/vandborg/webuntis/MainActivity.java
@@ -1,7 +1,13 @@
 package ucn.datamatiker.vandborg.webuntis;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +16,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Document doc;
+		try {
+			doc = Jsoup.connect("https://82.103.142.37/WebUntis/Timetable.do?school=ucn&onlyTimetable=true&ajaxCommand=renderTimetable&date=20130304&type=1&id=1570&formatId=1&departmentId=0&buildingId=-1&dojo.preventCache=1361793330275").userAgent("Mozilla").get();
+			Log.w("WebUntis", doc.html().toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Log.w("WebUntis", e.toString());
+			Log.w("WebUntis", e.getMessage());
+		}
 	}
 
 	@Override
